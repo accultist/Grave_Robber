96e04623a86de13d846fffae795672ac970dafa6
kathy <yuenk@sonoma.edu>
2023-04-24 22:58:17 -0700

    deleted generic enemy and implemented first skeleton

---
+++ Source/Grave_Robber/CorpseSwordCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyCharacter.h"
+#include "CorpseSwordCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRAVE_ROBBER_API ACorpseSwordCharacter : public AEnemyCharacter
+{
+	GENERATED_BODY()
+	
+};
